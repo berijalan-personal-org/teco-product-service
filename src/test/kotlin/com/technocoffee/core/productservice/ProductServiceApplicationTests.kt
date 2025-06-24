4ea668bd3d4aa73eab30d63af83c480b93058100
Patrik Kurniawan Saputra <surel.patrik@gmail.com>
2025-06-24 13:24:19 +0700

    Initial commit

---
+++ src/test/kotlin/com/technocoffee/core/productservice/ProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.technocoffee.core.productservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
